7d0131263359a076e7889b6988a4187c25a0f5f2
Grace <gracesilvaduarte@gmail.com>
2023-09-23 20:12:36 -0500

    se añadió el evento click para ocultar la lista de consolas disponible

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let menuDesplegable = document.getElementById("menuDesplegable");
+let verticalMenu = document.querySelector(".vertical-menu");
+
+function mostrarConsolas(){
+    verticalMenu.classList.toggle("hide");
+}
