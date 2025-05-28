5199659a77339f35e314cf5a9061a3cc26a80195
Grace <gracesilvaduarte@gmail.com>
2025-05-27 19:28:15 -0500

    Menú desplegable: El menú desplegable se despliega desde la parte superior de la pantalla, así mismo se corrigen los colores del sitio

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-let menuDesplegable = document.getElementById("menuDesplegable");
-let verticalMenu = document.querySelector(".vertical-menu");
+const menuConsolas = document.querySelector('.consoles-menu');
+const botonConsolas = document.querySelector('.consoles-button');
 
-function mostrarConsolas(){
-    verticalMenu.classList.toggle("hide");
-}
+// contraer el menú de las consolas al presionar el botón de las consolas:
+function mostrarConsolas() {
+    menuConsolas.classList.toggle('show');
+  }
+
+// Cierra el menú al hacer clic fuera
+document.addEventListener('click', (e) => {
+
+    if (!menuConsolas.contains(e.target) && !botonConsolas.contains(e.target)) {
+      menuConsolas.classList.remove('show');
+    }
+  });
+  // cerrar el menú con la tecla ESC:
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      menuConsolas.classList.remove('show');
+    }
+  });
